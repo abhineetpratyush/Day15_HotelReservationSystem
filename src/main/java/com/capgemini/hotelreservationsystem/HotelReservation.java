4e2627f0148952dc6e2edbf108264484f33fd493
abhineetpratyush <abhineetpratyush1998@gmail.com>
2020-10-18 10:41:24 +0530

    [abhineet] add : display welcome message in HotelReservation class

---
+++ src/main/java/com/capgemini/hotelreservationsystem/HotelReservation.java
@@ -0,0 +1,13 @@
+package com.capgemini.hotelreservationsystem;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class HotelReservation {
+	private static final Logger log = LogManager.getLogger(HotelReservation.class);
+
+	public static void main(String[] args) {
+		log.info("Welcome to Hotel Reservation Program");
+	}
+
+}
